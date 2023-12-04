931c628ec3645f6f885c353fea2781abd6bd6564
sba23313 <sba23313@student.cct.ie>
2023-12-04 21:23:32 +0000

    Created Car Class and properties

---
+++ src/classesandobjectsexercises/CarClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classesandobjectsexercises;
+
+/**
+ *
+ * @author palic
+ */
+public class CarClass {
+    
+}
