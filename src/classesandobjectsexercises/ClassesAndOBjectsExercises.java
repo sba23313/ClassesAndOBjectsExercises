6e845c6b27eaddc88938cdcd75b4858fca4d4551
sba23313 <sba23313@student.cct.ie>
2023-12-04 21:17:41 +0000

    Hello Class!

--- src/classesandobjectsexercises/ClassesAndOBjectsExercises.java
+++ src/classesandobjectsexercises/ClassesAndOBjectsExercises.java
@@ -15,6 +15,7 @@ public class ClassesAndOBjectsExercises {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Class!");
     }
     
 }
